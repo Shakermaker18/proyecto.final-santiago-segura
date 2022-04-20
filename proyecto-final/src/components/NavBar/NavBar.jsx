37408806dcb82db68decab8e77b701d24aa9facf
santiago segura <santiagodanielsegura@gmail.com>
2022-04-19 23:59:33 -0300

    Mi comit desafio E-Commerce

---
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NavBar.css";
+
+
+function NavBar(props) {
+    return (
+        <div>
+            <h1>MOTOCICLETAS</h1>
+            <ul className="nav">
+                <a href="#">Kawasaki</a>
+                <a href="#">Honda</a>
+                <a href="#">Susuki</a>
+                <a href="#">Yamaha</a>
+            </ul>    
+        </div>
+    )
+}
+
+export default NavBar; 
